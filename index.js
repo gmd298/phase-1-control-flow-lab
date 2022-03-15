db958e0c35da981026dab191f63a5d557499b451
Griffin <gmd298@gmail.com>
2022-03-14 20:14:41 -0400

    commit tree aee3e8699f357d205c5cd4bf7758eae27961b286

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return ('This one is on me!');
+  } else if (someValue > 2000, someValue < 2500) {
+      return ('I will gladly take your thirty bucks.');
+  } else if (someValue > 2500) {
+    return ('No can do.');
+  }
+}
+scuberGreetingForFeet(199);
+
+scuberGreetingForFeet(2001);
+
+scuberGreetingForFeet(2501);
+
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ternaryCheckCity('NYC')
+ternaryCheckCity('Pittsburgh')
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+
+    case 'not as generous':
+      return 'Thank you.';
+
+    default:
+      return 'Bye.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+//function switchOnCharmFromTip(tip){
+//  switch (tip) {
+//    case 'generous':
+//      tip >= 10;
+//      break;
+//    case 'not as generous'
+//      tip >= 5;
+//      break;
+//    default:
+//      case 'thanks for everything'
+//  }
+//}
